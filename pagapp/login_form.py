e61ad0bf681de21f96931c66819ca58c57c9cd17
h0rr0rrdrag0n <h0rr0rrdrag@gmail.com>
2015-02-18 23:14:53 +0000

    Replaced usual form inputs by WTForms inputs

---
+++ pagapp/login_form.py
@@ -0,0 +1,8 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(Form):
+    login = StringField('login', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
--- pagapp/views.py
+++ pagapp/views.py
@@ -1,10 +1,13 @@
 from flask import render_template
 from pagapp import app
+from .login_form import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
+    form = LoginForm()
     return render_template("index.html",
                            title="Test title",
+                           form=form,
                            user_logged_in=False)
