f9901fd7393ffdd33e4046dfe012544947b8a936
h0rr0rrdrag0n <h0rr0rrdrag@gmail.com>
2015-02-20 01:16:08 +0300

    Now login/pwd stored in string fields

--- config.py
+++ config.py
@@ -7,4 +7,3 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = 'eWax6eighodie7aiFeed'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'pagapp.db')
-SQLALCHEMY_DATABASE_REPO = os.path.join(basedir, 'db_repository')
