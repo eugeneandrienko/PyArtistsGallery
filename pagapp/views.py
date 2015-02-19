6e310b94fa550e0faba928b42cb0b742c4a4138f
h0rr0rrdrag0n <h0rr0rrdrag@gmail.com>
2015-02-19 00:13:11 +0000

    Added fake 'log in' for pressed sign in button. Just playing with WTForms

--- pagapp/views.py
+++ pagapp/views.py
@@ -3,11 +3,17 @@ from pagapp import app
 from .login_form import LoginForm
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
+    user_logged_in = False
     form = LoginForm()
+
+    if form.validate_on_submit():  # TODO: Add adequate user auth.
+        if form.login.data == 'user' and form.password.data == 'qwerty':
+            user_logged_in = True
+
     return render_template("index.html",
                            title="Test title",
                            form=form,
-                           user_logged_in=False)
+                           user_logged_in=user_logged_in)
