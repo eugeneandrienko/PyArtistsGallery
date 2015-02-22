5fc3e2246d26b7bba7742fd7bab3e03e61821b53
h0rr0rrdrag0n <h0rr0rrdrag@gmail.com>
2015-02-22 23:07:37 +0300

    Added warning message for user if he fails login procedure

--- pagapp/views.py
+++ pagapp/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from pagapp import app
 from pagapp import lm
 from pagapp.login_form import LoginForm
@@ -22,6 +22,11 @@ def login():
     if form.validate_on_submit():
         login_user(form.get_logged_in_user())
         return redirect(url_for('index'))
+    elif form.login.data is not None or form.password.data is not None:
+        # if some text entered in form, but we are here and not in
+        # /index page -- login seems failed and we should show warning
+        # message to user
+        flash('Login failed! Please double check your login name and password.')
 
     return render_template("login.html",
                            title=app.config['GALLERY_TITLE'],
