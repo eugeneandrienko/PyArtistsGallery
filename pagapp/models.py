95bef43e7c097c755e269cb3a092419a95b8e53a
h0rr0rrdrag0n <h0rr0rrdrag@gmail.com>
2015-03-05 15:46:26 +0300

    Added new tables for uploaded pictures

--- pagapp/models.py
+++ pagapp/models.py
@@ -87,3 +87,26 @@ class Albums(db.Model):
                 }
             )
         return result
+
+
+class Pictures(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    album_id = db.Column(db.Integer, db.ForeignKey('albums.id'))
+    path_to_image = db.Column(db.String(), index=True, nullable=False)
+    description = db.Column(db.String(), index=True, nullable=False)
+    upload_date = db.Column(db.DateTime(), index=True, nullable=False)
+    uploader_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __init__(self, album_id, uploader_id, upload_date, path_to_image, description=''):
+        self.album_id = album_id
+        self.path_to_image = path_to_image
+        self.description = description
+        self.upload_date = upload_date
+        self.uploader_id = uploader_id
+
+    def __repr__(self):
+        return 'Album ID: {}, Uploader ID: {}, Date: {}, Path: {}, Desc: {}'.format(
+            str(self.album_id), str(self.uploader_id),
+            str(self.upload_date),
+            self.path_to_image,
+            self.description)
