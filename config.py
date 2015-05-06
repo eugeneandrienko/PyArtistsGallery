c0bb8be2ce44931ce90ce4b42c485b608b7d149f
h0rr0rrdrag0n <h0rr0rrdrag@gmail.com>
2015-05-06 16:43:38 +0300

    Fix landscape.io error message about hardcoded secret key.
    Secret key for Flask-WTF Forms now generates in runtime.

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ SQLALCHEMY_DATABASE_URI -- absolute path to SQLite database.
 """
 
 import os
+import random
+import string
 
 
 _BASEDIR = os.path.abspath(os.path.dirname(__file__))
@@ -16,4 +18,8 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_BASEDIR, 'pagapp.db')
 
 # Specific settings for Flask-WTForm. Do not edit!
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'eWax6eighodie7aiFeed'
+SECRET_KEY_LENGTH = 30
+SECRET_KEY = ''.join(
+    random.choice(
+        string.ascii_letters + string.digits
+    ) for _ in range(SECRET_KEY_LENGTH))
